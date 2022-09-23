9d094abb967d5bfb5ffc5bd24b6892728be6b5f2
henriquecscode <henriquecostasousa.hcs@gmail.com>
2022-09-23 12:52:38 +0100

    1000 artists

---
+++ scrapings/1000_raw_artists.py
@@ -0,0 +1,8 @@
+# https://chartmasters.org/most-streamed-artists-ever-on-spotify/
+with open("1000_raw_artists.txt", "r", encoding='utf8') as f:
+    data = f.readlines()
+
+names = [record.split("\t")[1] for record in data]
+print(names)
+with open("1000_artists.txt", "w") as f:
+    f.write('\n'.join(names))
