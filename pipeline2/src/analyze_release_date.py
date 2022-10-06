3e53fb1496ef413be4f5b11a83dbb949134dd8b2
Henrique Nunes <hrn2001@gmail.com>
2022-10-06 12:17:05 +0100

    Process release date and analyze it

---
+++ pipeline2/src/analyze_release_date.py
@@ -0,0 +1,9 @@
+from sys import argv
+import matplotlib.pyplot as plt
+import pandas as pd
+
+filename = argv[1]
+
+df = pd.read_csv(filename, sep=';')
+plt.hist(df['album_release_date'].values, 10, rwidth=0.9)
+plt.show()
