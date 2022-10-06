89068cd5e6f4f0a37354f2558141c4339bf20737
Henrique Nunes <hrn2001@gmail.com>
2022-10-06 13:51:24 +0100

    DELETE OLD CODE

--- oldIdeas/scrapings/1000_raw_artists.py
+++
@@ -1,9 +0,0 @@
-# https://chartmasters.org/most-streamed-artists-ever-on-spotify/
-# 23/09/2022
-with open("1000_raw_artists.txt", "r", encoding='utf8') as f:
-    data = f.readlines()
-
-names = [record.split("\t")[1] for record in data]
-print(names)
-with open("1000_artists.txt", "w", encoding='utf8') as f:
-    f.write('\n'.join(names))
