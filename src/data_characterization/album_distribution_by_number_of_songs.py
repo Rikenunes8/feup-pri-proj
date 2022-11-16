a8c0137e1344a7331be8ebcd36c6da6198cd3bde
henriquecscode <henriquecostasousa.hcs@gmail.com>
2022-11-16 18:22:18 +0000

    album distribution by number of songs

---
+++ src/data_characterization/album_distribution_by_number_of_songs.py
@@ -0,0 +1,14 @@
+from sys import argv
+import matplotlib.pyplot as plt
+import pandas as pd
+
+filename = './processed/all.csv'
+
+df = pd.read_csv(filename, sep=';')
+data_by_album = df.groupby(['artist', 'album']).size()
+print(data_by_album)
+plt.hist(data_by_album, 20)
+plt.title("Album distribution by number of songs")
+plt.xlabel("Album mean track duration (min)")
+plt.ylabel('# of albums')
+plt.savefig('analysis/album_distribution_by_number_of_songs.png')
