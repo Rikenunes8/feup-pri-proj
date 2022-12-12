99b9dea66e9935eaacfeca1b98a9dd654a928012
Henrique Nunes <hrn2001@gmail.com>
2022-12-12 19:34:01 +0000

    Update docker to m3

--- solr/startup.sh
+++ solr/startup.sh
@@ -5,7 +5,8 @@ precreate-core tracks
 # Start Solr in background mode so we can use the API to upload the schema
 solr start
 
-# cp /data/synonyms_en.txt /var/solr/data/tracks/synonyms_en.txt
+cp /data/synonyms_en.txt /var/solr/data/tracks/synonyms_en.txt
+cp /data/synonyms_es.txt /var/solr/data/tracks/synonyms_es.txt
 
 # Schema definition via API
 curl -X POST -H 'Content-type:application/json' \
@@ -13,7 +14,7 @@ curl -X POST -H 'Content-type:application/json' \
     http://localhost:8983/solr/tracks/schema
 
 # Populate collection
-bin/post -c tracks /data/m2_tracks.json
+bin/post -c tracks /data/tracks.json
 
 # Restart in foreground mode so we can access the interface
 solr restart -f
