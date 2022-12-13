81f37baca8f7bfdb83d09306f3500f084dc551c3
Henrique Nunes <hrn2001@gmail.com>
2022-12-13 16:09:10 +0000

    Fix average precision

---
+++ run_evaluate.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+python3 solr/evaluate.py 1 simple_
+python3 solr/evaluate.py 2 simple_
+python3 solr/evaluate.py 3 simple_
+python3 solr/evaluate.py 4 simple_
+python3 solr/evaluate.py 5 simple_
+python3 solr/evaluate.py 6 simple_
+python3 solr/evaluate.py 7 simple_
+python3 solr/evaluate.py 8 simple_
